d4dbcac423488205f6b67c63c4155516a1ffa9de
lagushonok2000 <vorobjeva.an4stasiy@yandex.ru>
2023-10-06 23:48:05 +0400

    webversion

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -3,21 +3,34 @@ using UnityEngine;
 public class LevelSystem : MonoBehaviour
 {
     [SerializeField] private Hold _hold;
-    [SerializeField] private LevelPositiitions _changeLevelsPositions;
+    //[SerializeField] private LevelPositiitions _changeLevelsPositions;
     [SerializeField] public int[] Levels = { };
-    [SerializeField] private GameObject[] _accessories;
+    [SerializeField] public GameObject[] _accessories;
     [SerializeField] private GameObject _endFace;
     [SerializeField] private GameObject _monsterFace;
+    [SerializeField] private GameObject _panelGameOver;
+    [SerializeField] private GameObject _levelSystemPanel;
     
-
     public void LevelChek(float TotalPoints)
     {
+        if (TotalPoints == 0)
+        {
+            GameOver();
+        }
+
+        /*if (_endFace.active)
+        {
+            _panelGameOver.SetActive(true);
+            _levelSystemPanel.SetActive(false);
+        }
+        */
+
         for (int i = 0; i < Levels.Length; i++)
         {
             if (TotalPoints >= Levels[i])
             {
                 _accessories[i].SetActive(true);
-                _changeLevelsPositions.ChangeLevelsPositions(i);
+                //_changeLevelsPositions.ChangeLevelsPositions(i);
 
                 if (i == 4)
                 {
@@ -35,12 +48,13 @@ public class LevelSystem : MonoBehaviour
         {
             _endFace.SetActive(false);
             _monsterFace.SetActive(true);
+
             for (int i = 0;i < _accessories.Length;i++)
             {
                 _accessories[i].SetActive(false);
             }
 
-            _changeLevelsPositions.ChangeLevelsPositions(0);
+            //_changeLevelsPositions.ChangeLevelsPositions(0);
         }
     }
 }
